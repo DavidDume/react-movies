a642c43aba8f8efe1d7b4240df7234283a92d601
David Dume <dumedavid11@gmail.com>
2024-01-14 17:20:36 +0100

    api call for popular movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react"
 import Main from "./components/Main"
 import Navbar from "./components/Navbar"
 import SearchBar from "./components/SearchBar"
+import Trending from "./components/Trending"
+
+import { Movie } from "../types/Movie"
 
 function App() {
+    const baseUrl = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_MOVIEDB_API_KEY}`,
+        },
+    }
+
+    const [popularMovies, setPopularMovies] = useState<Movie[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(baseUrl, options)
+                const data = await res.json()
+                setPopularMovies(data.results)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <>
             <div className="flex">
@@ -12,6 +40,7 @@ function App() {
                 <div className="w-11/12">
                     <Main>
                         <SearchBar />
+                        <Trending movies={popularMovies} />
                     </Main>
                 </div>
             </div>
