71c8f9fc54bc647f7874db1dbf936f5a7c3ff7c5
David Dume <dumedavid11@gmail.com>
2024-01-14 15:01:45 +0100

    created UI for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,27 @@
-const Navbar = () => {
-  return <div className="">Navbar</div>;
-};
+import { FaUikit, FaBorderAll, FaFilm, FaTv, FaBookmark } from "react-icons/fa"
 
-export default Navbar;
+const Navbar = () => {
+    const navItems = [<FaBorderAll />, <FaFilm />, <FaTv />, <FaBookmark />]
+
+    return (
+        <div className="text-white m-10">
+            <div className="flex flex-col items-center bg-[#171D2F] rounded-lg h-[100vh]">
+                <div className="text-red-500 text-4xl my-4">
+                    <FaUikit />
+                </div>
+                <ul className="text-xl">
+                    {navItems.map((item, index) => (
+                        <li
+                            className="my-10 hover:cursor-pointer hover:text-white text-[#63688D]"
+                            key={index}
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
